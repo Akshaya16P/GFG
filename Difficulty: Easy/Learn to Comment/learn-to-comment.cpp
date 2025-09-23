4f4c205b7d58744f7939928e434d70f7ab8a210b
Akshaya16P <155613057+Akshaya16P@users.noreply.github.com>
2025-09-23 07:13:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment/learn-to-comment.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    void printValues(int a, int b, int c) {
+        // code here
+        //  a=5,b=6,c=15;
+        cout << a << " ";
+        // cout << b << " ";
+        cout << c << " ";
+    }
+};
