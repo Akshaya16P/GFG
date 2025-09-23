6f0929c112667e65afd26de755ef2a837101308c
Akshaya16P <155613057+Akshaya16P@users.noreply.github.com>
2025-09-23 18:33:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int x,y;
+    cin>>x>>y;
+    int p=x+y;
+    int q=x-y;
+    int r=x*y;
+    int s=x/y;
+    int t=x%y;
+    cout<<p<<" ";
+    cout<<q<<" ";
+    cout<<r<<" ";
+    cout<<s<<" ";
+    cout<<t;
+
+    return 0;
+}
