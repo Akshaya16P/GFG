bfadddedbadf87eed93a5c7d84fc0a76fda01730
Akshaya16P <155613057+Akshaya16P@users.noreply.github.com>
2025-12-17 20:56:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/If conditional statement- Python/if-conditional-statement-python.py
+++ Difficulty: Easy/If conditional statement- Python/if-conditional-statement-python.py
@@ -1,5 +1,7 @@
 def friends_in_trouble(j_angry, s_angry):
-    if j_angry==s_angry:
-        return "True"
+    if j_angry==True and s_angry==True:
+        return True
+    if j_angry==False and s_angry==False:
+        return True
     else:
-        return "False"
+        return False
