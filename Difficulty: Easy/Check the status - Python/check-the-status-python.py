df75054eb0da91b59d6ca997a42df4a5ac9c6b5e
Akshaya16P <155613057+Akshaya16P@users.noreply.github.com>
2025-12-17 20:52:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check the status - Python/check-the-status-python.py
+++ Difficulty: Easy/Check the status - Python/check-the-status-python.py
@@ -1,9 +1,11 @@
 class Solution:
-    def checkStatus(self, a: int, b: int, flag: bool) -> bool:
-        one_non_negative = (a >= 0) != (b >= 0)
-        
-        cond_1 = one_non_negative and not flag
-
-        cond_2 = (a < 0 and b < 0) and flag
-
-        return cond_1 or cond_2
+    def checkStatus(self, a, b, flag):
+        # code here
+          if a<0 and b<0 and flag==True:
+              return True
+          elif a<0 and b>0 and flag==False:
+              return True
+          elif a>0 and b<0 and flag==False:
+              return True
+          else:
+               return False
