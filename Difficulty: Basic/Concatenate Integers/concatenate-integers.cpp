a4805efbd58bfab106834b9e82eec0c82bd741f8
Akshaya <155613057+Akshaya16P@users.noreply.github.com>
2026-02-09 21:17:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Concatenate Integers/concatenate-integers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    // Write Your Code to Concate and Print
+    
+    cout<<a<<""<<b;
+    
+    return 0;
+}
+
