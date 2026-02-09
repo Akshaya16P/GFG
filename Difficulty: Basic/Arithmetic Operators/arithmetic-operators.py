76b6724ce4e7b17844ac911b66db849a26da94b4
Akshaya <155613057+Akshaya16P@users.noreply.github.com>
2026-02-09 21:01:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.py
@@ -0,0 +1,11 @@
+x=int(input())
+y=int(input())
+#code here
+# Perform addition x+y below
+
+p = (x + y) #: Addition
+q = (x - y) #: Subtraction
+r = (x * y) #:Multiplication
+s = (x // y) #: Division
+t = (x % y) #: Modulo
+print(p, q, r, s, t)
