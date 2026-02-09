69ee237a094f0b10cb0145fc88fb8fc6bc2fe6db
Akshaya <155613057+Akshaya16P@users.noreply.github.com>
2026-02-09 21:09:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.py
@@ -0,0 +1,12 @@
+#User function Template for python3
+#User function Template for python3
+a = int(input())
+
+if (a % 3 == 0) and (a % 5 == 0):
+    print("FizzBuzz")
+elif a % 5 == 0:
+    print("Buzz")
+elif a % 3 == 0:
+    print("Fizz")
+else:
+    print(a)
