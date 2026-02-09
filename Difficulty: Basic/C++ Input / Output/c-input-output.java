22f6ce48a1115b9a9b224ecb85e6c271b53eb2aa
Akshaya <155613057+Akshaya16P@users.noreply.github.com>
2026-02-09 21:07:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Input / Output/c-input-output.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    static int multiplication(int A, int B) {
+        return A*B;
+    }
+     public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+         int A=sc.nextInt();
+         int B=sc.nextInt();
+         Solution ob=new Solution();
+         System.out.println(ob.multiplication(A,B));
+         
+     }  
+}
